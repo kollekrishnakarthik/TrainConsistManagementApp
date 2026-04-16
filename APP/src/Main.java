c434e7b68d197a6a5c1f892683b12ee02fe1d4b3
kollekrishnakarthik <kolle.krishnakarthik@gmail.com>
2026-04-16 11:59:23 +0530

    Sort Bogie Names Using Arrays.sort()

--- APP/src/Main.java
+++ APP/src/Main.java
@@ -1,26 +1,17 @@
+import java.util.Arrays;
+
 public class Main {
 
-    public static void bubbleSort(int[] arr) {
-        int n = arr.length;
-
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = 0; j < n - i - 1; j++) {
-                if (arr[j] > arr[j + 1]) {
-                    int temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
-                }
-            }
-        }
+    public static String[] sortBogieNames(String[] names) {
+        Arrays.sort(names);
+        return names;
     }
 
     public static void main(String[] args) {
-        int[] capacities = {72, 56, 24, 70, 60};
+        String[] bogies = {"Sleeper", "AC Chair", "First Class", "General", "Luxury"};
 
-        bubbleSort(capacities);
+        sortBogieNames(bogies);
 
-        for (int c : capacities) {
-            System.out.print(c + " ");
-        }
+        System.out.println(Arrays.toString(bogies));
     }
 }
